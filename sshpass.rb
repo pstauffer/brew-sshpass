defc568a80465df85bf2e43e89f0e9d5f098d152
Pascal Stauffer <pstauffer@confirm.ch>
2019-02-03 01:56:18 +0100

    Fix order of the version definition

--- sshpass.rb
+++ sshpass.rb
@@ -1,8 +1,8 @@
 class Sshpass < Formula
   desc "noninteractive ssh password provider"
   homepage "http://sshpass.sourceforge.net/"
-  url "https://netix.dl.sourceforge.net/project/sshpass/sshpass/#{version}/sshpass-#{version}.tar.gz"
   version "1.06"
+  url "https://netix.dl.sourceforge.net/project/sshpass/sshpass/#{version}/sshpass-#{version}.tar.gz"
   sha256 "c6324fcee608b99a58f9870157dfa754837f8c48be3df0f5e2f3accf145dee60"
 
   def install

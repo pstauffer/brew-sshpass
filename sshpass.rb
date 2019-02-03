444f28709f5e7b2a7a27826eb45f9bb99157f9e2
Pascal Stauffer <pstauffer@confirm.ch>
2019-02-03 01:54:21 +0100

    Fix sourceforge download url

--- sshpass.rb
+++ sshpass.rb
@@ -1,7 +1,7 @@
 class Sshpass < Formula
   desc "noninteractive ssh password provider"
   homepage "http://sshpass.sourceforge.net/"
-  url "https://downloads.sourceforge.net/projects/sshpass/files/sshpass/#{version}/sshpass-#{version}.tar.gz"
+  url "https://netix.dl.sourceforge.net/project/sshpass/sshpass/#{version}/sshpass-#{version}.tar.gz"
   version "1.06"
   sha256 "c6324fcee608b99a58f9870157dfa754837f8c48be3df0f5e2f3accf145dee60"
 
